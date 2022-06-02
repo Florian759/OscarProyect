6499cec38d7988ea632148131d63e52c2ec60603
Florian Gonzalez <florian.gonzalez@alma3d.com>
2022-06-02 23:29:31 +0200

    wip

--- prueba.sql
+++ prueba.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1:3306
--- Tiempo de generación: 01-06-2022 a las 19:18:23
+-- Tiempo de generación: 02-06-2022 a las 21:29:05
 -- Versión del servidor: 5.7.36
 -- Versión de PHP: 7.4.26
 
@@ -35,14 +35,7 @@ CREATE TABLE IF NOT EXISTS `adopciones` (
   PRIMARY KEY (`id`),
   KEY `FK_perro` (`perro_id`),
   KEY `FK_usuario` (`usuario_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
-
---
--- Volcado de datos para la tabla `adopciones`
---
-
-INSERT INTO `adopciones` (`id`, `usuario_id`, `perro_id`) VALUES
-(1, 1, 1);
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -76,21 +69,21 @@ DROP TABLE IF EXISTS `perros`;
 CREATE TABLE IF NOT EXISTS `perros` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `nombre` varchar(30) DEFAULT NULL,
-  `description` mediumtext NOT NULL,
+  `description` mediumtext,
   `raza` int(30) DEFAULT NULL,
   `edad` int(11) DEFAULT NULL,
   `foto` varchar(50) CHARACTER SET utf16 COLLATE utf16_spanish_ci DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `raza` (`raza`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `perros`
 --
 
 INSERT INTO `perros` (`id`, `nombre`, `description`, `raza`, `edad`, `foto`) VALUES
-(1, 'Ñigu', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eleifend auctor sapien, rhoncus auctor arcu. Ut in felis sem. Proin nec eros eu erat consectetur egestas a vel augue. Nullam vestibulum, magna in convallis scelerisque, nisl nisl pellentesque lectus, nec hendrerit dui mi nec turpis. Nunc ultrices purus eget dui pretium, in feugiat metus malesuada. Cras felis leo, scelerisque at porta id, interdum eu mi. Praesent non facilisis leo. Aliquam erat volutpat. Donec a sagittis ex. In cursus congue nibh sit amet cursus. Aenean fringilla nulla sed massa maximus vehicula.', 1, 3, './assets/images/perroheader2.jpg'),
-(2, 'Ñigu 2', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eleifend auctor sapien, rhoncus auctor arcu. Ut in felis sem. Proin nec eros eu erat consectetur egestas a vel augue. Nullam vestibulum, magna in convallis scelerisque, nisl nisl pellentesque lectus, nec hendrerit dui mi nec turpis. Nunc ultrices purus eget dui pretium, in feugiat metus malesuada. Cras felis leo, scelerisque at porta id, interdum eu mi. Praesent non facilisis leo. Aliquam erat volutpat. Donec a sagittis ex. In cursus congue nibh sit amet cursus. Aenean fringilla nulla sed massa maximus vehicula.', 2, 3, './assets/images/perroheader2.jpg');
+(1, '&Ntilde;igu', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eleifend auctor sapien, rhoncus auctor arcu. Ut in felis sem. Proin nec eros eu erat consectetur egestas a vel augue. Nullam vestibulum, magna in convallis scelerisque, nisl nisl pellentesque lectus, nec hendrerit dui mi nec turpis. Nunc ultrices purus eget dui pretium, in feugiat metus malesuada. Cras felis leo, scelerisque at porta id, interdum eu mi. Praesent non facilisis leo. Aliquam erat volutpat. Donec a sagittis ex. In cursus congue nibh sit amet cursus. Aenean fringilla nulla sed massa maximus vehicula.', 1, 3, './assets/images/perroheader2.jpg'),
+(2, '&Ntilde;igu 2', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eleifend auctor sapien, rhoncus auctor arcu. Ut in felis sem. Proin nec eros eu erat consectetur egestas a vel augue. Nullam vestibulum, magna in convallis scelerisque, nisl nisl pellentesque lectus, nec hendrerit dui mi nec turpis. Nunc ultrices purus eget dui pretium, in feugiat metus malesuada. Cras felis leo, scelerisque at porta id, interdum eu mi. Praesent non facilisis leo. Aliquam erat volutpat. Donec a sagittis ex. In cursus congue nibh sit amet cursus. Aenean fringilla nulla sed massa maximus vehicula.', 2, 3, './assets/images/perroheader2.jpg');
 
 -- --------------------------------------------------------
 
@@ -124,21 +117,20 @@ CREATE TABLE IF NOT EXISTS `usuarios` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `nombre` varchar(30) NOT NULL,
   `apellidos` varchar(50) NOT NULL,
+  `telefono` varchar(15) NOT NULL,
+  `email` varchar(50) NOT NULL,
   `password` varchar(255) NOT NULL,
   `isadmin` int(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `usuarios`
 --
 
-INSERT INTO `usuarios` (`id`, `nombre`, `apellidos`, `password`, `isadmin`) VALUES
-(1, 'ADMIN', 'sin apellidos', 'd404559f602eab6fd602ac7680dacbfaadd13630335e951f097af3900e9de176b6db28512f2e000b9d04fba5133e8b1c6e8df59db3a8ab9d60be4b97cc9e81db', 1),
-(11, 'UBU', '', 'ubu', 0),
-(12, 'UBU', '', 'ubu', 0),
-(15, 'intento2', '', 'd404559f602eab6fd602ac7680dacbfaadd13630335e951f097af3900e9de176b6db28512f2e000b9d04fba5133e8b1c6e8df59db3a8ab9d60be4b97cc9e81db', 0),
-(16, 'testing', '', '521b9ccefbcd14d179e7a1bb877752870a6d620938b28a66a107eac6e6805b9d0989f45b5730508041aa5e710847d439ea74cd312c9355f1f2dae08d40e41d50', 1);
+INSERT INTO `usuarios` (`id`, `nombre`, `apellidos`, `telefono`, `email`, `password`, `isadmin`) VALUES
+(1, 'ADMIN', 'sin apellidos', '6000000', 'admin@test.com', 'd404559f602eab6fd602ac7680dacbfaadd13630335e951f097af3900e9de176b6db28512f2e000b9d04fba5133e8b1c6e8df59db3a8ab9d60be4b97cc9e81db', 1),
+(22, 'Test', 'ape', '0000', 'test@test.com', 'd404559f602eab6fd602ac7680dacbfaadd13630335e951f097af3900e9de176b6db28512f2e000b9d04fba5133e8b1c6e8df59db3a8ab9d60be4b97cc9e81db', 0);
 
 --
 -- Restricciones para tablas volcadas
